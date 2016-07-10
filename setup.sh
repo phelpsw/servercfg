f7fc1395c1d87133df52bb646f53e26aef2e6593
Phelps Williams <phelpsw@gmail.com>
2016-07-10 13:12:35 -0700

    Fixing standalone server configuration, certbot cronjob and non-interactive setup.

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ make test
 SYNCHOST="argus.williamslabs.com"
 KEY=`head -c 20 /dev/urandom | sha1sum | awk '{print $1}'`
 sed "s|host = 0.0.0.0|host = 127.0.0.1|" <syncserver.ini >tmp.ini
-sed "s|public_url = http://localhost:5000/|public_url = https://$SYNCHOST/|" <syncserver.ini >tmp.ini
-sed 's|#sqluri = sqlite:////tmp/syncserver.db|sqluri = sqlite:////tmp/syncserver.db|' <tmp.ini >tmp2.ini
+sed "s|public_url = http://localhost:5000/|public_url = https://$SYNCHOST/|" <tmp.ini >tmp1.ini
+sed 's|#sqluri = sqlite:////tmp/syncserver.db|sqluri = sqlite:////tmp/syncserver.db|' <tmp1.ini >tmp2.ini
 sed "s/#secret = INSERT_SECRET_KEY_HERE/secret = $KEY/" <tmp2.ini >syncserver.ini
-rm tmp.ini tmp2.ini
+rm tmp.ini tmp1.ini tmp2.ini
 
 cat > /etc/init/syncserver.conf <<'endmsg'
 description "Mozilla Firefox sync server"
@@ -61,12 +61,15 @@ ln -s /etc/nginx/sites-available/syncserver /etc/nginx/sites-enabled/default
 cd /home/ubuntu
 wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
-./certbot-auto certonly --standalone -d argus.williamslabs.com
+./certbot-auto --non-interactive --agree-tos --email admin@williamslabs.com certonly --standalone -d argus.williamslabs.com
+
+# Add certbot cronjob to ubuntu crontab
+crontab -l -u root > /tmp/crondump
+echo "17 * * * * /home/ubuntu/certbot-auto renew --standalone --non-interactive --pre-hook 'service nginx stop' --post-hook 'service nginx start' --quiet" >> /tmp/crondump
+crontab -u root /tmp/crondump
+rm /tmp/crondump
 
 # Start service
 start syncserver
 service nginx start
-
-# TODO add cronjob
-# certbot renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start"
 
