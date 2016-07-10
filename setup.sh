f751889074a907baa466c88a153559ff271f71e3
Phelps Williams <phelpsw@gmail.com>
2016-07-10 08:24:45 -0700

    Some directory cleanup.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/mozilla-services/syncserver
-cd syncserver
+git clone https://github.com/mozilla-services/syncserver /home/ubuntu/syncserver
+cd /home/ubuntu/syncserver
 make build
 make test
 
+echo "working directory"
+pwd
+
 # TODO: fix public url to match reality
 # TODO: Generate key
-sed 's|public_url = http://localhost:5000/|public_url = http://localhost:5000/|' <syncserver.ini >syncserver.ini
-sed 's|#sqluri = sqlite:////tmp/syncserver.db|sqluri = sqlite:////tmp/syncserver.db|' <syncserver.ini >syncserver.ini
+sed 's|public_url = http://localhost:5000/|public_url = http://localhost:5000/|' <syncserver.ini >tmp.ini
+sed 's|#sqluri = sqlite:////tmp/syncserver.db|sqluri = sqlite:////tmp/syncserver.db|' <tmp.ini >syncserver.ini
 
+# Ideally would create upstart file here but to avoid 
 cat > /etc/init/syncserver <<'endmsg'
 description "Mozilla Firefox sync server"
 
