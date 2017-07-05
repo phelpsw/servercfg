d9ca5251b33cd5029b64fa2980d167a16d91c802
Phelps Williams <phelpsw@gmail.com>
2017-07-04 17:00:05 -0700

    A bunch of improvements to the runner

--- run.sh
+++ run.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
 
-instance_id=$(aws ec2 run-instances --image-id ami-d732f0b7 --count 1 --instance-type t2.micro --key-name bravo --network-interfaces '[ { "NetworkInterfaceId": "eni-ce006cb3", "DeviceIndex": 0 } ]' --user-data=file://cloud-config --query 'Instances[0].InstanceId')
+# Parameters
+AMI="ami-835b4efa"
+KEY="supernova_5_12_2017"
+ENI="eni-ce006cb3"
+
+# TODO: Delete old ebs volume
+
+#
+# Terminate previous instance if ENI is still attached
+#
+old_instance=$(aws ec2 describe-network-interface-attribute \
+    --network-interface-id eni-ce006cb3 \
+    --attribute attachment \
+    --query Attachment.InstanceId)
+if [ $old_instance != "null" ]; then
+    echo "Found existing instance $old_instance with ENI attached, terminating"
+    # Strip quotes from around the instance id
+    old_instance=$(sed -e 's/^"//' -e 's/"$//' <<<"$old_instance")
+
+    # Terminate the instance
+    aws ec2 terminate-instances --instance-ids $old_instance
+
+    term_state=""
+    while [[ $term_state != "null" ]]
+    do
+        sleep 10
+        term_state=$(aws ec2 describe-network-interface-attribute \
+            --network-interface-id eni-ce006cb3 \
+            --attribute attachment \
+            --query Attachment.InstanceId)
+        echo "Waiting for instance shutdown"
+    done
+fi
+
+#
+# ENI Should now be available
+#
+instance_id=$(aws ec2 run-instances --image-id ${AMI} \
+    --count 1 --instance-type t2.micro \
+    --key-name ${KEY} --user-data=file://cloud-config \
+    --network-interfaces "[ { \"NetworkInterfaceId\": \"${ENI}\", \"DeviceIndex\": 0 } ]" \
+    --query 'Instances[0].InstanceId')
 
 if [ $? -ne 0 ]
 then
@@ -8,20 +49,24 @@ then
     exit 1
 fi
 
-echo "Created instance id"
-echo $instance_id
+echo "Created instance id $instance_id"
 
-# Wait until instance is running to attach volume
+# Wait until instance is running
 state=""
 while [[ $state != "running" ]]
 do
-    sleep 1
-    state=$(aws ec2 describe-instances --filter "Name=instance-id,Values=$instance_id" --query 'Reservations[0].Instances[0].State.Name' --output text)
+    sleep 5
+    state=$(aws ec2 describe-instances --filter "Name=instance-id,Values=$instance_id" \
+        --query 'Reservations[0].Instances[0].State.Name' --output text)
     echo $state
 done
 
-# TODO: lookup volume id based on tag
-
-aws ec2 attach-volume --volume-id vol-0783528f --instance-id $instance_id --device xvdb
-
+#
+# Print helpful login details
+#
+dnsname=$(aws ec2 describe-instances \
+    --filter "Name=instance-id,Values=$instance_id" \
+    --query 'Reservations[0].Instances[0].PublicDnsName')
+dnsname=$(sed -e 's/^"//' -e 's/"$//' <<<"$dnsname")
+echo "ssh -i ~/.ssh/${KEY}.pem ubuntu@${dnsname}"
 
