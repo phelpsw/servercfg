da95755f074f99ec3c0b8d1cf3c91914dfcba5d8
Phelps Williams <phelpsw@gmail.com>
2017-10-17 09:01:10 -0700

    Adding static route config on client

--- air2/setup.sh
+++ air2/setup.sh
@@ -4,3 +4,9 @@ aws s3 cp s3://phelps-swim-data/id_rsa.pub /tmp/
 cat /tmp/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 rm /tmp/id_rsa.pub
 
+cat >> /etc/network/if-up.d/static-route << 'EOF'
+#!/bin/bash
+
+/sbin/ip route add 155.178.68.32/27 via 172.16.1.10 dev eth0
+EOF
+chmod 755 /etc/network/if-up.d/static-route
